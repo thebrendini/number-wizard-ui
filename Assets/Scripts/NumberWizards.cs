d4996e79e3a7c6a11aa4ccd81c07739b35b0dae2
Brendan <McGinn>
2016-02-17 21:45:59 -0800

    Number Wizard UI
    User Interface

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+
+    public int maxGuessessAllowed = 6;
+    public Text text;
+
+    void Start () {
+        StartGame();
+	}
+	
+    void StartGame () {
+        max = 1000;
+        min = 1;
+        NextGuess();
+    }
+
+    public void GuessLower(){
+        max = guess;
+        NextGuess();
+    }
+
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max+1);
+        text.text = guess.ToString();
+        maxGuessessAllowed = maxGuessessAllowed - 1;
+        if(maxGuessessAllowed <= 0) {
+            Application.LoadLevel("Win");
+        }
+    }
+}
